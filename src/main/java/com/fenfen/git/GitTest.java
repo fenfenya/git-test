024f9327f4820cb2213237927181b599dab22072
fenfen <yqlmjhckn@163.com>
2022-09-01 17:25:32 +0800

    to GitHub commit

--- src/main/java/com/fenfen/git/GitTest.java
+++ src/main/java/com/fenfen/git/GitTest.java
@@ -6,7 +6,8 @@ public class GitTest {
         System.out.println("Hello,Git2!");
         System.out.println("Hello,Git3!");
         System.out.println("branch merge!");
-        System.out.println("branch  conflict master");
+        System.out.println("branch conflict master!");
         System.out.println("branch conflict hot-fix!");
+        System.out.println("commit to GitHub!");
     }
 }
